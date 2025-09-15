a256fdec2e2bfd347ede987a6290e51dc95913fe
StenixDev <stenix.dev@gmail.com>
2025-09-15 16:36:09 +0800

    controlled elemenents

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -24,22 +26,35 @@ function Logo(){
 
 function Form(){
 
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
   function handleSubmit(e){
+
     e.preventDefault();
-    alert('submited!')
+
+    if(!description.trim()) return
+
+    const newItem = {description, quantity, packed: false, id: Date.now()}
+
+    console.log(newItem)
+
+    setDescription('')
+    setQuantity(1)
+  
   }
 
 
   return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your trip? </h3>
-    <select>
+    <select onChange={(e) => setQuantity(Number(e.target.value))} value={quantity}>
       { 
       Array.from({length: 20}, (_, i) => i + 1)
       .map(num => <option key={num} value={num}>{num}</option>)
       }
     </select>
 
-    <input type="text" placeholder="item..." />
+    <input type="text" onChange={(e) => setDescription(e.target.value)} value={description} placeholder="item..." />
 
     <button>Add</button>
 
