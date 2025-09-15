5b9328bfb482f369fa1d397f0993c1099d20fa92
StenixDev <stenix.dev@gmail.com>
2025-09-15 15:39:11 +0800

    rendering the item list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 function App() {
 
@@ -24,7 +28,22 @@ function Form(){
   </div>
 }
 function PackingList(){
-  return <div className="list">LIST</div>
+  return <div  className="list"><ul>
+
+{
+  initialItems.map(item => <Item item={item} />)
+}
+    
+
+  </ul></div>
+}
+
+function Item({item}){
+  return <li>
+    <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
+    <button>❌</button>
+   
+  </li>
 }
 
 function Stats(){
