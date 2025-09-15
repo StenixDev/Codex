569d25ae6c51296c7801adf23888be87d2e524c5
StenixDev <stenix.dev@gmail.com>
2025-09-15 15:55:31 +0800

    building a form and handling submission

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
 ];
 
 function App() {
@@ -23,19 +23,37 @@ function Logo(){
 }
 
 function Form(){
-  return <div className="add-form">
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('submited!')
+  }
+
+
+  return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your trip? </h3>
-  </div>
+    <select>
+      { 
+      Array.from({length: 20}, (_, i) => i + 1)
+      .map(num => <option key={num} value={num}>{num}</option>)
+      }
+    </select>
+
+    <input type="text" placeholder="item..." />
+
+    <button>Add</button>
+
+  </form>
 }
 function PackingList(){
-  return <div  className="list"><ul>
-
+  return <div  className="list">
+    <ul>
 {
-  initialItems.map(item => <Item item={item} />)
-}
-    
+  initialItems.map(item => <Item item={item} key={item.id} />)
+} 
 
-  </ul></div>
+  </ul>
+  </div>
 }
 
 function Item({item}){
