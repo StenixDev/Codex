e956a1210a2b28fce45e50108cfd14a9c434d7cf
StenixDev <stenix.dev@gmail.com>
2026-01-18 14:01:04 +0800

    [vite] - installed Tooltip Card from aceternity UI

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,9 +1,24 @@
 import Card from './components/card';
+import { Tooltip } from '@/components/ui/tooltip-card';
 
 function App() {
   return (
-    <div className=''>
+    <div className='w-3xl mx-auto'>
       <h1 className='text-3xl font-bold text-center py-5'>App Vite</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        officia id ut facilis accusamus dolorem aspernatur aliquam, quidem,
+        nostrum temporibus atque rem modi hic iste maiores.{' '}
+        <Tooltip
+          containerClassName='text-neutral-600 dark:text-neutral-400'
+          content='words frequently found within Lorem ipsum placeholder text'
+        >
+          <span className='font-bold text-xl text-yellow-300'>
+            Totam consectetur
+          </span>
+        </Tooltip>
+        autem error?
+      </p>
       <div className='flex justify-center'>
         <Card
           title='The Title'
