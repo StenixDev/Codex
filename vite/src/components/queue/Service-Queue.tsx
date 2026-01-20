a35aa1e6818e3748b92dd816f60c149238c674c8
StenixDev <stenix.dev@gmail.com>
2026-01-20 14:08:44 +0800

    [vite] - QMS. statusQueue switch case

--- vite/src/components/queue/Service-Queue.tsx
+++ vite/src/components/queue/Service-Queue.tsx
@@ -12,26 +12,46 @@ type QueueItem = {
 };
 
 function ServiceQueue({ data, onUpdate }: QueueItem) {
-  function handleClick() {
-    console.log(data.id, 'working');
-    onUpdate(data.id, 'working');
-  }
+  const statusQueue = (status: string) => {
+    switch (status) {
+      case 'serving':
+        // code to run
+        return 'text-red-400';
+      case 'completed':
+        return 'text-yellow-500';
+      default:
+        return 'text-blue-400';
+    }
+  };
 
   return (
     <div className='flex justify-between items-center bg-stone-200 my-5 p-5'>
       <div className='flex flex-col'>
         <span className='font-bold text-xl'>{data.name}</span>
         <span>service: {data.service}</span>
-        <span className='text-blue-400 font-bold'>{data.status}</span>
+        <span className={`${statusQueue(data.status)} font-bold1`}>
+          {data.status}
+        </span>
       </div>
 
       <div className='flex gap-2'>
-        <Button
-          onClick={handleClick}
-          className='bg-green-500 hover:bg-green-600'
-        >
-          Serve
-        </Button>
+        {data.status === 'waiting' && (
+          <Button
+            onClick={() => onUpdate(data.id, 'serving')}
+            className='bg-green-500 hover:bg-green-600'
+          >
+            Serve
+          </Button>
+        )}
+
+        {data.status === 'serving' && (
+          <Button
+            onClick={() => onUpdate(data.id, 'completed')}
+            className='bg-green-500 hover:bg-green-600'
+          >
+            Completed
+          </Button>
+        )}
         <Button className='bg-red-500 hover:bg-red-600'>
           <UserRoundMinus />
         </Button>
