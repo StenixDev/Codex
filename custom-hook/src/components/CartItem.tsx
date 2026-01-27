a0916c5e7dd659d9f72660d3761196f12d87cb49
StenixDev <stenix.dev@gmail.com>
2026-01-27 17:18:55 +0800

    [custom-hooks] - Working on CartItem

--- custom-hook/src/components/CartItem.tsx
+++ custom-hook/src/components/CartItem.tsx
@@ -1,4 +1,28 @@
-function CartItem() {
-  return <div>CartItem</div>;
+import { Minus, Plus, Trash } from 'lucide-react';
+
+function CartItem({ item, onUpdateQuantity, onRemove }) {
+  return (
+    <div className='cart-item'>
+      <div className='item-details'>
+        <h4>{item.name}</h4>
+        <p>{item.price}</p>
+        <div className='quantity-controls'>
+          <button onClick={() => onUpdateQuantity(item.id, item.quantity - 1)}>
+            <Minus />
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button onClick={() => onUpdateQuantity(item.id, item.quantity + 1)}>
+            <Plus />
+          </button>
+        </div>
+      </div>
+
+      <button className='remove-btn' onClick={() => onRemove(item.id)}>
+        <Trash />
+      </button>
+    </div>
+  );
 }
 export default CartItem;
