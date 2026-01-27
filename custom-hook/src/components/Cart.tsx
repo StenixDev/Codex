d277602fc9d490ade25d1bba259b0f14ebfe48dc
StenixDev <stenix.dev@gmail.com>
2026-01-27 16:25:59 +0800

    [custom-hooks] - working on ProductCard and Cart

--- custom-hook/src/components/Cart.tsx
+++ custom-hook/src/components/Cart.tsx
@@ -1,4 +1,26 @@
-function Cart() {
-  return <div>Cart</div>;
+import CartItem from './CartItem';
+
+function Cart({ cart, onUpdateQuantity, onRemove, total }) {
+  if (cart.length === 0)
+    return <div className='cart empty'>Your Cart is Empty</div>;
+
+  return (
+    <div className='cart'>
+      <h2>Shopping Cart</h2>
+      {cart.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          onUpdateQuantity={onUpdateQuantity}
+          onRemove={onRemove}
+        />
+      ))}
+
+      <div className='cart-total'>
+        <h3>Total: {total}</h3>
+        <button className='checkout-btn'>Checkout</button>
+      </div>
+    </div>
+  );
 }
 export default Cart;
--- custom-hook/src/components/ProductCard.tsx
+++ custom-hook/src/components/ProductCard.tsx
@@ -1,4 +1,22 @@
-function ProductCard() {
-  return <div>ProductCard</div>;
+import { ShoppingCart } from 'lucide-react';
+type Product = {
+  name: string;
+  price: number;
+};
+type ProductCardProps = {
+  product: Product;
+  onAddToCart: (product: Product) => void;
+};
+
+function ProductCard({ product, onAddToCart }: ProductCardProps) {
+  return (
+    <div className='product-cart'>
+      <h3>{product.name}</h3>
+      <p className='price'>${product.price}</p>
+      <button onClick={() => onAddToCart(product)}>
+        <ShoppingCart /> Add to Cart
+      </button>
+    </div>
+  );
 }
 export default ProductCard;
