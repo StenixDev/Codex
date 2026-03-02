3e2e5629a41a4ae0bd3b3c44195474444f587622
StenixDev <stenix.dev@gmail.com>
2026-03-02 19:59:05 +0800

    [CRUD] - update REST and add form edit

--- crud/app/api/notes/[id]/route.ts
+++ crud/app/api/notes/[id]/route.ts
@@ -4,6 +4,40 @@ import Note from '@/models/Notes';
 
 import { NextResponse, NextRequest } from 'next/server';
 
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const { id } = params;
+
+    const body = await request.json();
+    const { title, content } = body;
+
+    await dbConnect();
+
+    const updatedNote = await Note.findByIdAndUpdate(
+      id,
+      { title, content, updatedAt: new Date() },
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedNote) {
+      return NextResponse.json(
+        { success: false, error: 'Note not found' },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({
+      success: true,
+      data: updatedNote,
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error }, { status: 400 });
+  }
+}
+
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } },
--- crud/components/NotesClient.tsx
+++ crud/components/NotesClient.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { Types } from 'mongoose';
+import type { SubmitEvent } from 'react';
 import toast from 'react-hot-toast';
 
 interface Note {
@@ -21,11 +22,12 @@ function NotesClient({ noteInit }: NotesClientProps) {
   const [notes, setNotes] = useState([...noteInit]);
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
+  const [noteId, setNoteId] = useState<Types.ObjectId | null>(null);
+  const [editTitle, setEditTitle] = useState<string>('');
+  const [editContent, setEditContent] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
-  console.log(notes);
-
-  async function createNote(e) {
+  async function createNote(e: SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
 
     if (!title.trim() || !content.trim()) return;
@@ -49,6 +51,7 @@ function NotesClient({ noteInit }: NotesClientProps) {
         toast.success('notes successfully addded');
       }
     } catch (error) {
+      throw error;
       toast.error('There is an error submitting your note');
     } finally {
       setLoading(false);
@@ -71,6 +74,16 @@ function NotesClient({ noteInit }: NotesClientProps) {
       console.error('error deleted note', error);
       toast.error('something went wrong');
     }
+  }
+
+  function setStartEdit(note: Note) {
+    setNoteId(note._id);
+    setEditTitle(note.title);
+    setEditContent(note.content);
+  }
+
+  async function updateNote(noteId) {
+    console.log(noteId);
   }
 
   return (
@@ -106,33 +119,80 @@ function NotesClient({ noteInit }: NotesClientProps) {
 
       <div className='space-y-4'>
         <h2 className='text-xl font-semibold'>your notes {notes.length}</h2>
+
         {notes.length === 0 ? (
           <p className='text-gray-500'>
             no notes yet. create your first note above
           </p>
         ) : (
-          notes.map((note) => (
-            <div
-              key={note._id.toString()}
-              className='bg-white p-5 rounded-lg shadow-md'
-            >
-              <div className='flex justify-between items-start mb-2'>
-                <h3 className='text-lg font-semibold'>{note.title}</h3>
-                <div className='flex gap-2'>
-                  <button className='text-blue-500 hover:text-blue-700 text-sm cursor-pointer'>
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => deleteNote(note._id)}
-                    className='text-red-500 hover:text-red-700 text-sm  cursor-pointer'
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+          notes.map((note) =>
+            note._id === noteId ? (
+              <div key={note._id} className='bg-white p-6 rounded-lg shadow-md'>
+                <form onSubmit={() => updateNote(note._id)}>
+                  <h2 className='font-bold mb-5'>Edit note</h2>
 
-              <p className='text-gray-700 mb-2'>{note.content}</p>
-              <div>
+                  <div className='space-y-4'>
+                    <input
+                      type='text'
+                      className='border-2 rounded-md w-full p-2'
+                      placeholder='Title'
+                      value={editTitle}
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      required
+                    />
+
+                    <textarea
+                      placeholder='Content'
+                      value={editContent}
+                      onChange={(e) => setEditContent(e.target.value)}
+                      rows={4}
+                      className='border-2 rounded-md w-full p-2'
+                    />
+
+                    <button
+                      type='submit'
+                      disabled={loading}
+                      className='bg-black text-white px-4 py-2 mr-2'
+                    >
+                      {loading ? 'updating...' : 'Update'}
+                    </button>
+
+                    <button
+                      type='button'
+                      onClick={() => setNoteId(null)}
+                      className='bg-black text-white px-4 py-2'
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
+            ) : (
+              <div
+                key={note._id.toString()}
+                className='bg-white p-5 rounded-lg shadow-md'
+              >
+                <div className='flex justify-between items-start mb-2'>
+                  <h3 className='text-lg font-semibold'>{note.title}</h3>
+
+                  <div className='flex gap-2'>
+                    <button
+                      onClick={() => setStartEdit(note)}
+                      className='text-blue-500 hover:text-blue-700 text-sm'
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => deleteNote(note._id)}
+                      className='text-red-500 hover:text-red-700 text-sm'
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+
+                <p className='text-gray-700 mb-2'>{note.content}</p>
+
                 <p className='text-gray-500 text-sm'>
                   Created: {new Date(note.createdAt).toLocaleString()}
                 </p>
@@ -143,8 +203,8 @@ function NotesClient({ noteInit }: NotesClientProps) {
                   </p>
                 )}
               </div>
-            </div>
-          ))
+            ),
+          )
         )}
       </div>
     </div>
