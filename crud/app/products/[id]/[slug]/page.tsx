36946b70f171f7c9491a51466514eea8ff5ea8bb
StenixDev <stenix.dev@gmail.com>
2026-03-04 06:10:26 +0800

    [exp] - useParams

---
+++ crud/app/products/[id]/[slug]/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
+function ProductPage() {
+  const params = useParams();
+
+  console.log(params);
+  return <div>ProductPage</div>;
+}
+export default ProductPage;
